e2af09b3152a7f6137a31d8339edec9e7f936d2d
Ben Harris <benjaminharris28@gmail.com>
2020-09-24 10:36:21 -0400

    added sort function, changed input to selector

--- src/CardList/CardList.js
+++ src/CardList/CardList.js
@@ -65,9 +65,102 @@ class CardList extends React.Component {
                         
 
                     
+                    <div className="title-centre">
+                            <div className="guide-heading">
+                            Live
+                            </div>
+                                
+                 
+                                <h1 className="display-heading small">
+                                    Updated in past hour
+                            </h1>
+                       
 
+
+
+                            </div>
                     <div className="card-loop">
-                    {this.state.waitTimes.map(wait => 
+                    {this.state.waitTimes.filter(w => w.avg_wait).sort((a,b) => (a.avg_wait > b.avg_wait) ? 1 : ((b.avg_wait > a.avg_wait) ? -1 : 0)).map(wait => 
+                         <div className="shop-product-card" key={wait.id}>
+                         <div className="shadow-box">
+                         <a target='_blank' rel="noopener noreferrer" href={`${wait.address_link}`}>
+            
+                         <div className="product-item">
+                           <div className="shop-product-image-box red">
+                               <div className={wait.avg_wait >= 90 ? "wait-time-image red" : "wait-time-image"}>
+                                   {wait.avg_wait ? 
+                                    <p className="wait-time-heading">{Math.round(wait.avg_wait)} minutes</p>
+                                    : <p className="wait-time-heading">No recent data</p>
+                                }
+                                
+                               </div>
+                            
+                              
+                               
+                           </div>
+                           <div className="shop-product-clickable-details">
+                          
+                               <p className="shop-product-title bold-title">{wait.name}</p>
+                           
+                           </div>
+               
+                           </div>
+                       
+                       
+                       </a>
+                       <div className="price-box">
+                         
+                       <a target='_blank' rel="noopener noreferrer" href={`${wait.address_link}`}>
+                           <p className="shop-product-detail">{wait.address}</p>
+                            </a>
+                       </div>
+                   
+                       <div className="shop-product-quick-add-box">
+               
+                                                               
+               
+               <Collapse 
+                   bordered={false}
+                   ghost>
+                   <Panel header="More Details" key="1">
+                       <div className="more-detail-panel">
+                        <p className="shop-product-detail">Hours: {wait.hours}</p>
+                        <p className="shop-product-detail">Age Restrictions: {wait.age_restrictions}</p>
+                        <a target='_blank' rel="noopener noreferrer" href={`${wait.link}`}>
+                        <p className="shop-product-detail">Site</p>
+                        </a>
+                       
+                       </div>
+                   </Panel>
+               </Collapse>
+               
+               </div>
+                       
+                       
+                      
+                     </div>
+                     </div>
+                    )}
+                    
+                </div>
+
+                <div className="title-centre">
+                            <div className="guide-heading">
+                            No data
+                            </div>
+                                
+                 
+                                <h1 className="display-heading small">
+                                    Waiting for updates
+                            </h1>
+                       
+
+
+
+                            </div>
+
+                <div className="card-loop">
+                    {this.state.waitTimes.filter(w => w.avg_wait === null).map(wait => 
                          <div className="shop-product-card" key={wait.id}>
                          <div className="shadow-box">
                          <a target='_blank' rel="noopener noreferrer" href={`${wait.address_link}`}>
@@ -80,6 +173,7 @@ class CardList extends React.Component {
                                     : <p className="wait-time-heading">No recent data</p>
                                 }
                                </div>
+                               
                                
                            </div>
                            <div className="shop-product-clickable-details">
--- src/WaitForm/WaitForm.js
+++ src/WaitForm/WaitForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Select, InputNumber, Button, Alert } from 'antd';
+import { Select, InputNumber, Button, Alert, Result } from 'antd';
 import { CheckCircleTwoTone } from '@ant-design/icons';
 import WaitApiService from '../services/wait-api-service';
 import * as dayjs from 'dayjs'
@@ -19,6 +19,16 @@ class WaitForm extends React.Component {
         })
     }
 
+    handleChangeTime = (value) => {
+        console.log(`selected ${value}`);
+
+        var waitValue = Number(value)
+
+        this.setState({
+            wait: waitValue
+        })
+    }
+
     onChange = (value) => {
         this.setState({
             wait: value
@@ -32,37 +42,37 @@ class WaitForm extends React.Component {
     }
 
     handleSubmit = () => {
-       const { location_id, wait } = this.state;
+        const { location_id, wait } = this.state;
 
-    
-       if (location_id > 0) {
-       const today = dayjs();
-        const date = dayjs(today).format('YYYY-MM-D')
-        const hour = dayjs(today).format('HH')
-        const prevHour = hour - 1
 
-        WaitApiService.createWait(date, hour, location_id, wait)
-        .then(res => {
-            this.props.refreshData()
+        if (location_id > 0) {
+            const today = dayjs();
+            const date = dayjs(today).format('YYYY-MM-D')
+            const hour = dayjs(today).format('HH')
+            const prevHour = hour - 1
 
-            this.setState({
-                submitted: true
-            }, () => {
-                setTimeout(() => {
+            WaitApiService.createWait(date, hour, location_id, wait)
+                .then(res => {
+                    this.props.refreshData()
+
                     this.setState({
-                        submitted: false,
-                
+                        submitted: true
                     }, () => {
-                        this.setState({
-                            formOpen: false
-                        })
+                        setTimeout(() => {
+                            this.setState({
+                                submitted: false,
+
+                            }, () => {
+                                this.setState({
+                                    formOpen: false
+                                })
+                            })
+                        }, 3500)
                     })
-                }, 3500)
-            })
-        })
-    } else {
-        alert("Please select a location");
-    }
+                })
+        } else {
+            alert("Please select a location");
+        }
 
     }
 
@@ -80,78 +90,96 @@ class WaitForm extends React.Component {
         return (
             <div className="wait-form">
                 <div className="form">
-                
+
                     <div className="content-container low-padding">
-                        {this.state.formOpen === false && 
-                        <div className="form-input">
+                        {this.state.formOpen === false &&
+                            <div className="form-input">
 
-                        
-                  
-                        <p className="form-label">Are you at an assessement centre?</p>
-                        <div className="button-box">
-                        <Button className="button"
-                            onClick={this.handleClick}>
-                            Report Wait Time
+
+
+                                <p className="form-label">Are you at an assessement centre?</p>
+                                <div className="button-box">
+                                    <Button className="button"
+                                        onClick={this.handleClick}>
+                                        Report Wait Time
                         </Button>
-                        </div>
-                    
-                        
+                                </div>
 
-                    
-                </div>
+
+
+
+                            </div>
                         }
 
-              
-                
-    {this.state.formOpen && this.state.submitted === false &&
-                    
-                <div className="select-form">
-                    <div className="location-input">
-                    <p className="form-label">Which assessment center are you at?</p>
-                    <Select className="selector" defaultValue="Select a location" style={{ width: "90%" }} onChange={this.handleChange}>
-                        <Option value="1">Etobicoke General Hospital Drive-Thru</Option>
-                        <Option value="2">Humber River Hospital, Reactivation Care Centre – Finch Site</Option>
-                        <Option value="3">Michael Garron Hospital</Option>
-                        <Option value="4">Mount Sinai Hospital</Option>
-                        <Option value="5">North York General Hospital</Option>
-                        <Option value="6">North York General Hospital – Branson Site</Option>
-                        <Option value="7">Scarborough Health Network – Birchmount Hospital</Option>
-                        <Option value="8">Scarborough Health Network – Centenary Hospital</Option>
-                        <Option value="9">Sunnybrook Health Sciences Centre</Option>
-                        <Option value="10">UHN Toronto Western Hospital</Option>
-                        <Option value="11">Unity – St. Joseph’s Health Centre</Option>
-                        <Option value="12">Unity – St. Michael’s Hospital</Option>
-                        <Option value="13">Women’s College Hospital</Option>
-                    </Select>
-                    </div>
 
-                    <div className="wait-time-input">
-                    <p className="form-label">What is the current estimated wait time?</p>
-                    
-                    <InputNumber className="wait-input" min={1} max={300} defaultValue={20} onChange={this.onChange} />
-                    {` minutes`}
-                    </div>
-              
-                    <div className="button-box">
-                    <Button className="button"
-                        onClick={(e) => this.handleSubmit(e)}>
-                        Submit Wait Time Report
+
+                        {this.state.formOpen && this.state.submitted === false &&
+
+                            <div className="select-form">
+                                <div className="location-input">
+                                    <p className="form-label">Which assessment center are you at?</p>
+                                    <Select className="selector" defaultValue="Select a location" style={{ width: "90%" }} onChange={this.handleChange}>
+                                        <Option value="1">Etobicoke General Hospital Drive-Thru</Option>
+                                        <Option value="2">Humber River Hospital, Reactivation Care Centre – Finch Site</Option>
+                                        <Option value="3">Michael Garron Hospital</Option>
+                                        <Option value="4">Mount Sinai Hospital</Option>
+                                        <Option value="5">North York General Hospital</Option>
+                                        <Option value="6">North York General Hospital – Branson Site</Option>
+                                        <Option value="7">Scarborough Health Network – Birchmount Hospital</Option>
+                                        <Option value="8">Scarborough Health Network – Centenary Hospital</Option>
+                                        <Option value="9">Sunnybrook Health Sciences Centre</Option>
+                                        <Option value="10">UHN Toronto Western Hospital</Option>
+                                        <Option value="11">Unity – St. Joseph’s Health Centre</Option>
+                                        <Option value="12">Unity – St. Michael’s Hospital</Option>
+                                        <Option value="13">Women’s College Hospital</Option>
+                                    </Select>
+                                </div>
+
+                                <div className="wait-time-input">
+                                    <p className="form-label">What is the current estimated wait time?</p>
+                                    <Select className="selector" defaultValue="Select a wait time" style={{ width: "90%" }} onChange={this.handleChangeTime}>
+                                        <Option value="15">Less than 15 mins</Option>
+                                        <Option value="30">15 - 30 mins</Option>
+                                        <Option value="45">30 - 45 mins</Option>
+                                        <Option value="60">1 hour</Option>
+                                        <Option value="75">1 hour - 1.5 hours</Option>
+                                        <Option value="105">1.5 hours - 2 hours</Option>
+                                        <Option value="120">2 hours</Option>
+                                        <Option value="180">3 hours</Option>
+                                        <Option value="240">4 hours</Option>
+                                        <Option value="300">More than 4 hours</Option>
+                                        <Option value="11">Unity – St. Joseph’s Health Centre</Option>
+                                        <Option value="12">Unity – St. Michael’s Hospital</Option>
+                                        <Option value="13">Women’s College Hospital</Option>
+                                    </Select>
+
+                                   
+                                </div>
+
+                                <div className="button-box">
+                                    <Button className="button"
+                                        onClick={(e) => this.handleSubmit(e)}>
+                                        Submit Wait Time Report
                     </Button>
+                                </div>
+                            </div>
+                        }
+                        <div className="thank-you-message">
+                            {this.state.submitted &&
+                                <div>
+                                    <Result
+                                        status="success"
+                                        title="Successfully submitted wait-time report"
+                                        
+                                       
+                                    />
+                                </div>
+
+                            }
+                        </div>
+                    </div>
                 </div>
-                </div>
-                }
-                 <div className="thank-you-message">
-                {this.state.submitted && 
-                <div>
-                    <CheckCircleTwoTone className="thank-you-icon" twoToneColor="#52c41a" />
-                    <p className="thank-you-heading">Thank you for submitting a wait time</p>
-                </div>
-                
-                }
-            </div>
-            </div>
-            </div>
-           
+
             </div>
         )
     }
