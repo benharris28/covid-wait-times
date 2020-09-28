9a3504c3edd2bf1a0c54ba82b2ed23e6dca9000a
Ben Harris <benjaminharris28@gmail.com>
2020-09-27 23:18:39 -0400

    updated loader

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 class Loader extends React.Component {
-    render() {
-        return (
-            <div>loaderio-dafa03867ad7f94a65d12cf61eaa47c0</div>
-        )
-    }
-}
+ 
+         render() {
+             return (
+                <div>loaderio-dafa03867ad7f94a65d12cf61eaa47c0</div>
+             )
+         }
+              
+        }
 
 export default Loader;
